aabca9ae97c8d8760dc5ad6253b685c479757b12
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-07 17:06:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+const Person = function(name, birthYear){
+    this.name = name;
+    this.birthYear = birthYear;
+};
+
+const currentPerson = new Person('Dima', 1980);
+console.log(currentPerson);
