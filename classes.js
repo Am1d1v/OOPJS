0bec8524cc48f2e476a9906010a94f1a2160445a
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-09 00:30:01 +0300

    Add files via upload

--- classes.js
+++ classes.js
@@ -4,19 +4,44 @@
 
 
 
+/*
+
 class Person{
 
     constructor(name, birthYear) {
         this.name = name;
         this.birthYear = birthYear;
     }
+
+    printAge(){
+        console.log(2023 - this.birthYear);
+    }
 }
 
 const Dima = new Person("Dima", 1990);
-console.log(Dima);
+console.log(Dima.printAge());
 
+console.log(Dima.__proto__ === Person.prototype);
 
+*/
 
+// Setters & Getters
 
+const account = {
+    owner: "Dima",
+    transactions: [100, -200, 300],
+
+    get lastTransaction(){
+        return this.transactions.slice(-1).pop();
+    },
+
+    set lastTransaction(transaction){
+        this.transactions.push(transaction);
+    }
+}
+
+//console.log(account.getLastTransaction());
+account.lastTransaction = 1000;
+console.log(account.transactions);
 
 
