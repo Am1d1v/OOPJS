d81d2e9befc813d22b82c3d808b7407571e31ce5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-08 15:03:34 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,42 @@
 const Person = function(name, birthYear){
     this.name = name;
     this.birthYear = birthYear;
+
+    
 };
 
-const currentPerson = new Person('Dima', 1980);
-console.log(currentPerson);
+const definitePerson = new Person('Dima', 1980);
+console.log(definitePerson);
+console.log(definitePerson instanceof Person);
+
+Person.prototype.printAge = function(){
+    console.log(2023 - this.birthYear);
+}
+/*
+
+console.log(Person.prototype);
+
+definitePerson.printAge();
+console.log(definitePerson.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(definitePerson));
+
+*/
+
+console.log(definitePerson.__proto__.__proto__);
+console.log(definitePerson.hasOwnProperty('name'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
