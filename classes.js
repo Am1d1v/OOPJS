3e6c4ffc730fc6339b33d96b293af31d16c1e72c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-08 16:00:54 +0300

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+class Person{
+
+    constructor(name, birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+}
+
+const Dima = new Person("Dima", 1990);
+console.log(Dima);
+
+
+
+
+
+
