9392810050a1603c65b486da43d86a0c83a51467
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-08-08 15:14:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -27,13 +27,21 @@ console.log(Person.prototype.isPrototypeOf(definitePerson));
 
 */
 
+console.log(definitePerson.__proto__);
 console.log(definitePerson.__proto__.__proto__);
 console.log(definitePerson.hasOwnProperty('name'));
+console.dir(Person.prototype.constructor);
 
+const numbers = [1, 2, 3, 4, 5, 6];
+const numbersArray = [1, 4, 3, 9, 5, 6, 5, 2];
+console.log(numbers.__proto__);
+console.log(numbers.__proto__ === Array.prototype);
 
+Array.prototype.uniqueElements = function(){
+    return [...new Set(this)]
+}
 
-
-
+console.log(numbersArray.uniqueElements());
 
 
 
